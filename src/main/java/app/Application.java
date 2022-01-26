882ff824ce0c26a33fb6a2f2de2985b950039271
alexey korneev <korneevkot@yandex.ru>
2022-01-26 11:00:27 +0300

    delete import

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,9 +1,7 @@
 package app;
 
 import app.config.AppConfig;
-import app.model.Animal;
 import app.model.AnimalsCage;
-import app.model.Dog;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
